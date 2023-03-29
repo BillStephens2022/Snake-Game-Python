36c639d81d37c473759fa69c7560e5fe2fd97396
BillStephens2022 <two4onebill@yahoo.com>
2023-03-29 09:07:29 -0400

    refactored code to use OOP - created Snake class

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from turtle import Turtle, Screen
+from snake import Snake
 import time
+
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -7,27 +9,13 @@ screen.bgcolor("black")
 screen.title("Python Snake Game")
 screen.tracer(0)
 
-starting_positions = [(0, 0), (-20, 0), (-40, 0)]
-segments = []
-
-for position in starting_positions:
-    new_segment = Turtle("square")
-    new_segment.color("white")
-    new_segment.penup()
-    new_segment.goto(position)
-    segments.append(new_segment)
+snake = Snake()
 
 game_is_on = True
 
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-    for seg in segments:
-        seg.penup()
-        seg.forward(20)
-        
-     
-
-
-
+    snake.move()
+    
 screen.exitonclick()
