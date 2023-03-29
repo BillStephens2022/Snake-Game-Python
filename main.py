132d4ce1b25ea5fda184aef8da7a537ef255bc6f
BillStephens2022 <two4onebill@yahoo.com>
2023-03-29 10:28:03 -0400

    added method to grow the snake as it eats food

--- main.py
+++ main.py
@@ -31,12 +31,15 @@ while game_is_on:
     #Detect collision of snake head with food
     if snake.head.distance(food) < 15:
         food.refresh()
+        snake.extend()
         scoreboard.increase_score()
 
     #Detect collision of snake with wall
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
         game_is_on = False
         scoreboard.game_over()
+
+    #Detect collision of snake with its own body
 
     
 screen.exitonclick()
